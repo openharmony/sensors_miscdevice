b9b5c7208278a11348681f05a4ee159ef2a03be7
hui1975 <zhaominghui10@huawei.com>
2023-08-01 09:00:38 +0000

    告警修改
    Signed-off-by: hui1975 <zhaominghui10@huawei.com>
    Change-Id: Id1876d98662a276eb4f227051a21f808c29a0fe6

--- services/miscdevice_service/test/fuzztest/vibratoronremoterequest_fuzzer/vibratoronremoterequest_fuzzer.cpp
+++ services/miscdevice_service/test/fuzztest/vibratoronremoterequest_fuzzer/vibratoronremoterequest_fuzzer.cpp
@@ -50,9 +50,6 @@ void SetUpTestCase()
     if (perms == nullptr) {
         return;
     }
-    if (sizeof(perms) / sizeof(perms[0]) < 1) {
-        return;
-    }
     perms[0] = "ohos.permission.VIBRATE";
     TokenInfoParams infoInstance = {
         .dcapsNum = 0,
