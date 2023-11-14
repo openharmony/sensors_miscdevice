2835d848372b652c0e383bc1377b3a63d6772e98
rwx1283129 <renxinyao2@huawei.com>
2023-11-14 01:22:44 +0000

    Isolate the user/eng version of the miscdevice bin
    Signed-off-by: rwx1283129 <renxinyao2@huawei.com>
    Change-Id: If2f252da31aa1dc70f91be2a046e0d5de2923de6

--- services/miscdevice_service/hdi_connection/interface/src/light_hdi_connection.cpp
+++ services/miscdevice_service/hdi_connection/interface/src/light_hdi_connection.cpp
@@ -16,7 +16,9 @@
 
 #include <list>
 
+#ifdef BUILD_VARIANT_ENG
 #include "compatible_light_connection.h"
+#endif
 #ifdef HDF_DRIVERS_INTERFACE_LIGHT
 #include "hdi_light_connection.h"
 #endif // HDF_DRIVERS_INTERFACE_LIGHT
@@ -41,7 +43,9 @@ int32_t LightHdiConnection::ConnectHdi()
         return ERR_OK;
     }
 #endif // HDF_DRIVERS_INTERFACE_LIGHT
+#ifdef BUILD_VARIANT_ENG
     iLightHdiConnection_ = std::make_unique<CompatibleLightConnection>();
+#endif
     return ConnectHdiService();
 }
 
