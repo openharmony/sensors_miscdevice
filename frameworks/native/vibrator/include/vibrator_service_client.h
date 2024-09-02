469c3a183fbca9608c30bb4ea3e8309e216d311b
tu-xinxin <xxtud@isoftstone.com>
2024-09-02 11:25:15 +0800

    miscdevice解耦hitrace
    Signed-off-by: tu-xinxin <xxtud@isoftstone.com>

--- frameworks/native/vibrator/include/vibrator_service_client.h
+++ frameworks/native/vibrator/include/vibrator_service_client.h
@@ -69,6 +69,8 @@ public:
     int32_t GetDelayTime(int32_t &delayTime);
     int32_t InitPlayPattern(const VibratorPattern &pattern, int32_t usage, bool systemUsage,
         const VibratorParameter &parameter);
+    int32_t InitPlayPattern(const VibratorPattern &pattern, int32_t usage, bool systemUsage,
+        const VibratorParameter &parameter);
     int32_t PlayPattern(const VibratorPattern &pattern, int32_t usage, bool systemUsage,
         const VibratorParameter &parameter);
     int32_t FreeVibratorPackage(VibratorPackage &package);
