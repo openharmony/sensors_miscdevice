e5be3d8990d4b9ed3367c05e7a01ed2a9bb862de
cff-gite <chenfeifei8@huawei.com>
2024-10-15 09:25:43 +0800

    马达维测日志打印
    Signed-off-by: cff-gite <chenfeifei8@huawei.com>
    Change-Id: I82ba408c43d5a3a44a5ec6f182d8faa333a491e0

--- services/miscdevice_service/src/miscdevice_service.cpp
+++ services/miscdevice_service/src/miscdevice_service.cpp
@@ -472,7 +472,7 @@ int32_t MiscdeviceService::IsSupportEffect(const std::string &effect, bool &stat
     state = effectInfo->isSupportEffect;
     std::string packageName = GetPackageName(GetCallingTokenID());
     std::string curVibrateTime = GetCurrentTime();
-    MISC_HILOGI("Stop vibrator, currentTime:%{public}s, package:%{public}s, pid:%{public}d, effect:%{public}s"
+    MISC_HILOGI("IsSupportEffect, currentTime:%{public}s, package:%{public}s, pid:%{public}d, effect:%{public}s"
         "state:%{public}d", curVibrateTime.c_str(), packageName.c_str(), GetCallingPid(), effect.c_str(), state);
 #endif // HDF_DRIVERS_INTERFACE_VIBRATOR
     return NO_ERROR;
