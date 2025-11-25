4f2d9f23019ae6400d52bd459745e3c41c5c36fd
wuzhihuitmac <wuzhihui4@huawei.com>
2025-11-25 16:34:56 +0800

    modify check opinion
    Signed-off-by: wuzhihuitmac <wuzhihui4@huawei.com>
    Change-Id: Icc0bafca8d254c0450ff1258e0e10bf7205c9a49

--- services/miscdevice_service/src/vibration_priority_manager.cpp
+++ services/miscdevice_service/src/vibration_priority_manager.cpp
@@ -366,7 +366,7 @@ int32_t VibrationPriorityManager::UnregisterUser100Observer()
     std::lock_guard<std::mutex> currentUserObserverLock(currentUserObserverMutex_);
     std::string callingIdentity = IPCSkeleton::ResetCallingIdentity();
     std::string tableType = "system";
-    auto helper = CreateDataShareHelper(ReplaceUserIdForUri(SETTING_USER_URI_PROXY, g_currentUserId.load()), tableType);
+    auto helper = CreateDataShareHelper(SETTING_USER_URI_PROXY, tableType);
     if (helper == nullptr) {
 #ifdef HIVIEWDFX_HISYSEVENT_ENABLE
         HiSysEventWrite(HiSysEvent::Domain::MISCDEVICE, "DATASHARE_EXCEPTION", HiSysEvent::EventType::FAULT,
